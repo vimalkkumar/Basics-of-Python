7c7da3f8dd218ab9e3095a9c8798b77f6fd4f356
Vimal Kumar <vimal15786@gmail.com>
2020-12-21 18:10:41 +0530

    Add files via upload

---
+++ standradPyLibrary.py
@@ -0,0 +1,36 @@
+# import random
+# print(dir())
+
+# help(dir)
+# help(random.randint)
+# print(random.randint(1, 20))
+
+# i = 1
+# for m in dir(__builtins__):
+#     print("{} - {}".format(i, m))
+#     i = i +1
+
+# print(dir(__builtins__.ArithmeticError))
+
+# for m in dir(__cached__):
+#     print(m)
+
+# for m in dir(__doc__):
+#     print(m)
+
+# for m in dir(__file__):
+#     print(m)
+
+# for m in dir(__loader__):
+#     print(m)
+
+# for m in dir(__name__):
+#     print(m)
+
+# for m in dir(__package__):
+#     print(m)
+
+# for m in dir(__spec__):
+#     print(m)
+
+
