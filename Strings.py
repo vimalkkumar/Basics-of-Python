b5d78f999715ba1899094944c189e77b980e9413
Vimal Kumar <vimal15786@gmail.com>
2020-12-21 18:11:42 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,26 @@
+print("Hello World!!!!!")
+print('Hello World!!!!!')
+print("Hello" + ' ' + "World")
+print("Hello" + "World")
+# Comment line
+print(75+12)
+print(12*12)
+print(12/2)
+print(55-5)
+splitStringDoubleQuote = """Hi,
+My self Vimal Kumar, CTO at DeScite.
+And I am from Kanpur India
+So, let's talk about little bit more about python"""
+print(splitStringDoubleQuote)
+
+splitStringSingleQuote = '''Hi,
+My self Vimal Kumar, CTO at DeScite.
+And I am from Kanpur India
+So, let's talk about python'''
+print(splitStringSingleQuote)
+
+totalVoter = 12457865
+appearVoter = 10245861
+percentage = appearVoter / totalVoter
+print("Total voter in the city is {} and the percentage of appeared "
+      "voter is {:2.2%}".format(totalVoter, percentage))
