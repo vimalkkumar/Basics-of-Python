5a76d07e146e93fabe4fa3e7edf5268f0f33d4ce
Vimal Kumar <vimal15786@gmail.com>
2020-12-23 20:12:31 +0530

    Add files via upload

---
+++ Python Standard Library/Introduction.py
@@ -0,0 +1,16 @@
+"""
+* A library that can be use on any machine where you have an application using the specific language.
+* Includes built-in functions and built-in imported modules
+* A standard is a built-in.
+* A external library is something you need to download separately from the Internet to get a special functionally.
+
+"""
+
+print(min(min("Malik", "Mohan", "Albert")))
+# select the minimum from the given data ("Albert") than minimum in "Albert" which is "A"
+
+print(min("_Mohan__", "__Mohan"))
+
+list1 = [15, 12, 25, 45, 13]
+print(min(list1))
+
