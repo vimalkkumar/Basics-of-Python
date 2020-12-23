85457c1158f0b38bee6a00b9770e6744e03746d8
Vimal Kumar <vimal15786@gmail.com>
2020-12-23 20:10:34 +0530

    Add files via upload

---
+++ Python Standard Library/HTMLParserModule.py
@@ -0,0 +1,31 @@
+from html.parser import HTMLParser
+
+
+class HTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start tag: ", tag)
+        for attr in attrs:
+            print("attr: ", attr)
+
+    def handle_endtag(self, tag):
+        print("End tag: ", tag)
+
+    def handle_comment(self, data):
+        print("Comment: ", data)
+
+    def handle_data(self, data):
+        print("Data: ", data)
+
+
+parser = HTMLParser()
+parser.feed("<html><head><title>Coder</title></head><body><h1><!--hi-->I am a Coder</h1></body></html>")
+
+
+inputText = input("Put in HTML Code")
+parser.feed(inputText)
+
+with open("HTMLSampleFile.html", 'r') as htmlFile:
+    s = ""
+    for line in htmlFile:
+        s += line
+    parser.feed(s)
