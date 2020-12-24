b2b5ee9aead04bedf216e55f8545ea57e4fd9de9
Vimal Kumar <vimal15786@gmail.com>
2020-12-24 20:04:38 +0530

    Add files via upload

---
+++ Python Standard Library/StatisticsModule.py
@@ -0,0 +1,11 @@
+import statistics
+
+agesData = [10, 15, 18, 12, 14, 17, 12, 21, 20, 22]
+
+print(statistics.mean(agesData))
+print(statistics.median(agesData))
+print(statistics.mode(agesData))
+
+print(statistics.variance(agesData))
+print(statistics.stdev(agesData))
+
